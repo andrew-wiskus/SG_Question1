22b86ce4cc07e5660b5126d2afbac54d45473f3f
Andrew Wiskus <drew.wiskus@newlionlabs.com>
2017-06-30 16:40:21 -0500

    graph bar + scene controller, initial commit

---
+++ Assets/Scripts/GraphBar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GraphBar : MonoBehaviour
+{
+	[SerializeField] private Text m_bar_text;
+	private Image m_bar_image;
+
+	void OnEnable()
+	{
+		m_bar_image = GetComponent<Image>();
+	}
+
+	public void SetBarColor(Color color)
+	{
+		m_bar_image.color = color;
+	}
+
+	public void SetBarText(string text)
+	{
+		m_bar_text.text = text;
+	}
+}
---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+	[SerializeField] private GameObject m_graph_container;
+}
